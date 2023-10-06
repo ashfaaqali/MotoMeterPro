96e402a9c13d736857196ddd2043ce3f5212e8bd
Mohd Ashfaq <tbeast305@gmail.com>
2023-10-06 15:32:21 +0530

    access location

--- app/src/main/java/com/example/speedometerapp/MainActivity.kt
+++ app/src/main/java/com/example/speedometerapp/MainActivity.kt
@@ -8,23 +8,33 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.example.speedometerapp.databinding.ActivityMainBinding
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
     private lateinit var locationManager: LocationManager
     private lateinit var locationListener: LocationListener
+    private lateinit var binding: ActivityMainBinding
 
+    private lateinit var fineLocation: String
+    private lateinit var coarseLocation: String
+    private var granted = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        requestLocationPermission()
+
+        fineLocation = Manifest.permission.ACCESS_FINE_LOCATION
+        coarseLocation = Manifest.permission.ACCESS_COARSE_LOCATION
+        granted = PackageManager.PERMISSION_GRANTED
+
         // Initialize location manager and listener
         locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
-        locationListener = LocationListener { location -> // Calculate speed in meters per second
+
+        locationListener = LocationListener { location ->
+            // Calculate speed in meters per second
             if (location.hasSpeed()) {
                 val speed = location.speed * 3.6 // Convert to km/h
                 binding.speedTextView.text = "%.0f".format(speed)
@@ -33,36 +43,23 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        if (ActivityCompat.checkSelfPermission(this, fineLocation) != granted &&
+            ActivityCompat.checkSelfPermission(this, coarseLocation) != granted) {
+
+        }
         locationManager.requestLocationUpdates(
             LocationManager.GPS_PROVIDER,
             10, // Update every 1 second
             0f,   // Update whenever there's a location change
             locationListener
         )
-
-        checkPermission()
     }
 
-    private fun checkPermission() {
-        // Request location updates
-        if (ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            locationManager.requestLocationUpdates(
-                LocationManager.GPS_PROVIDER,
-                1000,
-                1f,
-                locationListener
-            )
-        } else {
-            // Request location permission
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                1
-            )
-        }
+    private fun requestLocationPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            0
+        )
     }
 }
