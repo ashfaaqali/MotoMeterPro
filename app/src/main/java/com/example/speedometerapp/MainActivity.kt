e2988ff42c8513dfc43c765284ca54b2644ac519
Mohd Ashfaq <tbeast305@gmail.com>
2023-10-08 11:40:13 +0530

    permission button added

--- app/src/main/java/com/example/speedometerapp/MainActivity.kt
+++ app/src/main/java/com/example/speedometerapp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.example.speedometerapp.databinding.ActivityMainBinding
@@ -24,14 +25,34 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        requestLocationPermission()
-
         fineLocation = Manifest.permission.ACCESS_FINE_LOCATION
         coarseLocation = Manifest.permission.ACCESS_COARSE_LOCATION
         granted = PackageManager.PERMISSION_GRANTED
 
         // Initialize location manager and listener
         locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+
+        binding.givePermissionBtn.setOnClickListener {
+            requestLocationPermission()
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    fineLocation
+                ) != granted && ActivityCompat.checkSelfPermission(this, coarseLocation) != granted){
+                requestLocationPermission()
+            }
+            if (ActivityCompat.checkSelfPermission(this, fineLocation) != granted || ActivityCompat.checkSelfPermission(this, coarseLocation) == granted) {
+                binding.errorLayout.visibility = View.VISIBLE
+            }
+            binding.permissionAlertLayout.visibility = View.GONE
+            binding.speedLayout.visibility = View.VISIBLE
+            locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                0, // Update every 1 second
+                0f,   // Update whenever there's a location change
+                locationListener
+            )
+        }
+
 
         locationListener = LocationListener { location ->
             // Calculate speed in meters per second
@@ -43,23 +64,26 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        if (ActivityCompat.checkSelfPermission(this, fineLocation) != granted &&
-            ActivityCompat.checkSelfPermission(this, coarseLocation) != granted) {
 
-        }
-        locationManager.requestLocationUpdates(
-            LocationManager.GPS_PROVIDER,
-            10, // Update every 1 second
-            0f,   // Update whenever there's a location change
-            locationListener
-        )
     }
 
     private fun requestLocationPermission() {
         ActivityCompat.requestPermissions(
             this,
-            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            arrayOf(fineLocation),
             0
         )
     }
+
+//    private fun requestLocationUpdates() {
+//        if (ActivityCompat.checkSelfPermission(this, fineLocation) != granted) {
+//            binding.errorLayout.visibility = View.VISIBLE
+//        }
+//        locationManager.requestLocationUpdates(
+//            LocationManager.GPS_PROVIDER,
+//            0, // Update every 1 second
+//            0f,   // Update whenever there's a location change
+//            locationListener
+//        )
+//    }
 }
