94fde323b25bee89755dc30ee7d0859c6324f7fa
Mohd Ashfaq <tbeast305@gmail.com>
2023-10-09 17:17:09 +0530

    fix bug

--- app/src/main/java/com/example/speedometerapp/MainActivity.kt
+++ app/src/main/java/com/example/speedometerapp/MainActivity.kt
@@ -56,9 +56,6 @@ class MainActivity : AppCompatActivity() {
         return (ActivityCompat.checkSelfPermission(
             this,
             Manifest.permission.ACCESS_FINE_LOCATION
-        ) == PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(
-            this,
-            Manifest.permission.ACCESS_COARSE_LOCATION
         ) == PackageManager.PERMISSION_GRANTED)
     }
 
